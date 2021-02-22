8965a8f86cd7dd9091b40307b6232185f65856c1
Evgeny Fimochkin <fimochkin@gmail.com>
2021-02-22 18:11:35 +0700

    first commit

---
+++ src/blocking_queue.h
@@ -0,0 +1,71 @@
+#pragma once
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <functional>
+#include <list>
+#include <map>
+#include <mutex>
+#include <queue>
+#include <shared_mutex>
+#include <thread>
+
+template<typename T>
+class BoundedBlockingQueue
+{
+public:
+  BoundedBlockingQueue(size_t max_capacity = 255)
+      : m_max_capacity(max_capacity)
+  {
+    //    m_data.resize(max_capacity);
+  }
+
+  ~BoundedBlockingQueue()
+  {
+    m_is_stopped = true;
+    m_cv_queue_empty.notify_all();
+    m_cv_queue_overflow.notify_all();
+  }
+
+  void add(const T& element)
+  {
+    //    std::shared_lock lock{ consumers_mtx };
+    std::unique_lock lock{ consumers_mtx };
+    m_cv_queue_overflow.wait(lock, [&]() { return m_data.size() < m_max_capacity; });
+
+    m_data.push(element);
+    lock.unlock();
+    m_cv_queue_empty.notify_all();
+  }
+
+  void add(T&& element)
+  {
+    std::unique_lock lock{ consumers_mtx };
+    m_cv_queue_overflow.wait(lock, [&]() { return m_data.size() < m_max_capacity; });
+
+    m_data.push(std::move(element));
+    lock.unlock();
+    m_cv_queue_empty.notify_all();
+  }
+
+  T take()
+  {
+    std::unique_lock lock{ consumers_mtx };
+    m_cv_queue_empty.wait(lock, [&]() { return !m_data.empty(); });
+
+    T n = m_data.front();
+    m_data.pop();
+    m_cv_queue_overflow.notify_all();
+    return n;
+  }
+
+private:
+  size_t m_max_capacity;
+  std::shared_mutex consumers_mtx;
+
+  std::queue<T> m_data;
+  std::condition_variable_any m_cv_queue_empty;
+  std::condition_variable_any m_cv_queue_overflow;
+  bool m_is_stopped = false;
+};
---
+++ tests/src/unit_tests.cxx
@@ -0,0 +1,207 @@
+#include <chrono>
+#include <condition_variable>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <mutex>
+#include <thread>
+#include <utility>
+#include <vector>
+
+#include <blocking_queue.h>
+auto sort_int = [](int i, int j) { return i < j; };
+
+TEST(SimpleAddTake, BoundedBlockingQueue)
+{
+  size_t max_size = 64;
+  BoundedBlockingQueue<int> queue(max_size);
+
+  std::vector<int> data;
+  data.resize(max_size);
+  for (int i = 0; i < max_size; i++)
+    data[i] = i;
+
+  for (auto n : data)
+    queue.add(n);
+
+  std::vector<int> grabbed_data;
+  for (int i = 0; i < max_size; i++) {
+    int n = queue.take();
+    grabbed_data.push_back(n);
+  }
+  std::sort(grabbed_data.begin(), grabbed_data.end(), sort_int);
+
+  EXPECT_THAT(data, ::testing::ContainerEq(grabbed_data));
+}
+
+class SingleBoundedBlockingQueue : public testing::TestWithParam<::std::tuple<int, int, bool>>
+{
+protected:
+  void SetUp() override {}
+  void TearDown() override {}
+};
+
+TEST_P(SingleBoundedBlockingQueue, ReaderLock)
+{
+  size_t max_size = std::get<0>(GetParam());
+  size_t element_count = get<1>(GetParam());
+  bool sync = get<2>(GetParam());
+
+  BoundedBlockingQueue<int> test(max_size);
+
+  std::vector<int> data;
+  data.resize(element_count);
+  for (int i = 0; i < element_count; i++)
+    data[i] = i;
+
+  std::vector<int> grabbed_data;
+
+  std::atomic<int32_t> flag;
+  flag.store(-1);
+
+  auto processor = [&] {
+    for (int i = 0; i < element_count; i++) {
+      int n = test.take();
+      if (sync) {
+        flag.store(i);
+      }
+      grabbed_data.push_back(n);
+    }
+  };
+
+  std::thread thread(processor);
+
+  while (flag != -1)
+    ;
+  for (int i = 0; i < element_count; i++) {
+    if (sync) {
+      if (i % 32) {
+        while (flag != (i - 1))
+          ; // blocks a thread to force the worker thread to wait for a new data
+      }
+    }
+    test.add(data[i]);
+  }
+
+  thread.join();
+
+  std::sort(grabbed_data.begin(), grabbed_data.end(), sort_int);
+
+  EXPECT_THAT(data, ::testing::ContainerEq(grabbed_data));
+}
+
+INSTANTIATE_TEST_CASE_P(BoundedBlockingQueue,
+                        SingleBoundedBlockingQueue,
+                        ::testing::Combine(::testing::Values(512), ::testing::Values(512, 1024), ::testing::Values(false, true)));
+
+TEST(QueueOverflow, BoundedBlockingQueue)
+{
+  size_t max_size = 128;
+  size_t element_count = 512;
+  bool sync = false;
+
+  BoundedBlockingQueue<int> queue(max_size);
+
+  std::vector<int> data;
+  data.resize(element_count);
+  for (int i = 0; i < element_count; i++)
+    data[i] = i;
+
+  std::vector<int> grabbed_data;
+
+  std::atomic<int32_t> flag;
+  flag.store(0);
+
+  auto processor = [&] {
+    for (int i = 0; i < element_count; i++) {
+      queue.add(data[i]);
+      flag++;
+    }
+  };
+
+  std::thread thread(processor);
+
+  while (flag < max_size)
+    ;
+  EXPECT_EQ(flag.load(), max_size);
+  for (int i = 0; i < element_count; i++) {
+    int n = queue.take();
+    if (sync) {
+      flag.store(i);
+    }
+    grabbed_data.push_back(n);
+  }
+
+  thread.join();
+
+  std::sort(grabbed_data.begin(), grabbed_data.end(), sort_int);
+
+  EXPECT_THAT(data, ::testing::ContainerEq(grabbed_data));
+}
+
+TEST(MultiThread, BoundedBlockingQueue)
+{
+  int max_size = 8;
+  int element_count = 64;
+  int producer_count = 4;
+  int consumer_count = 5;
+
+  BoundedBlockingQueue<int> queue(max_size);
+
+  std::vector<int> data;
+  data.resize(element_count);
+  for (int i = 0; i < element_count; i++)
+    data[i] = i;
+
+  auto producer = [&](int begin, int end) {
+    for (; begin < end; begin++) {
+      queue.add(data[begin]);
+    }
+  };
+
+  std::vector<std::thread*> producer_workers;
+  producer_workers.resize(producer_count);
+  int producer_chunk_size = element_count / producer_count;
+  for (int i = 0; i < producer_count; i++) {
+    int begin = producer_chunk_size * i;
+
+    int end = producer_chunk_size * (i + 1);
+    if ((producer_count - 1) == i)
+      end = element_count;
+    producer_workers[i] = new std::thread(producer, begin, end);
+  }
+
+  std::vector<int> grabbed_data;
+  std::mutex grabbed_data_mutex;
+  auto consumer = [&](int chunk_size) {
+    std::vector<int> data;
+    while (chunk_size--)
+      data.push_back(queue.take());
+
+    std::unique_lock lock(grabbed_data_mutex);
+    grabbed_data.insert(grabbed_data.end(), data.begin(), data.end());
+  };
+
+  int consumer_chunk_size = element_count / producer_count;
+  std::vector<std::thread*> consumer_workers;
+  consumer_workers.resize(consumer_count);
+  for (int i = 0; i < consumer_count; i++) {
+    int chunk_size = consumer_chunk_size;
+    if ((consumer_count - 1) == i)
+      chunk_size = element_count - consumer_chunk_size * i;
+    consumer_workers[i] = new std::thread(consumer, chunk_size);
+  }
+
+  for (auto p : producer_workers) {
+    p->join();
+    delete p;
+  }
+
+  for (auto c : consumer_workers) {
+    c->join();
+    delete c;
+  }
+
+  std::sort(grabbed_data.begin(), grabbed_data.end(), sort_int);
+
+  EXPECT_THAT(data, ::testing::ContainerEq(grabbed_data));
+}
